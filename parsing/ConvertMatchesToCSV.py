1b3844367c7e3c5f78bee810c72a9b82c5d69f61
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-07-10 15:31:38 -0400

    worked on parsing matches data into csv because tableau doesnt like json

---
+++ parsing/ConvertMatchesToCSV.py
@@ -0,0 +1,23 @@
+import json
+import csv
+
+MATCHES_JSON_DIR = "server/matches.json"
+OUTPUT_DIR = "parsing/matches.csv"
+
+ALLIANCES = ["redAlliance", "blueAlliance"]
+
+BASE_NAMES = ["matchNumber", "alliance", "teamNumber"]
+
+matchesJSON = json.load(open(MATCHES_JSON_DIR))["matches"]
+
+fullCSV = []
+fullCSV.append(BASE_NAMES)
+for matchNum, match in enumerate(matchesJSON):
+    for alliance in ALLIANCES:
+        for team in match[alliance]:
+            team = team.replace("frc", "")
+            fullCSV.append([matchNum, alliance, team])
+
+with open(OUTPUT_DIR, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(fullCSV)

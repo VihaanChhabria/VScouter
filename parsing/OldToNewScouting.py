3f94358c2c8cdeb247d7c3f2b27ee18e00406345
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-07-11 11:57:54 -0400

    fixed data that is not in old scouting data

--- parsing/OldToNewScouting.py
+++ parsing/OldToNewScouting.py
@@ -9,16 +9,16 @@ INPUT_CONVERSION = {
     "teamNumber" : "Match Team Number", 
     "initials" : "Initials", 
     "autoSpeakerMade" : "Auto Shots Made Speaker", 
-    "autoSpeakerMissed" : 0, 
+    "autoSpeakerMissed" : -1, 
     "autoAmpMade" : "Auto Shots Made Amp", 
-    "autoAmpMissed" : 0, 
+    "autoAmpMissed" : -1, 
     "teleopAmpMade" : "Teleop Shots Made Amp", 
-    "teleopAmpMissed" : 0, 
+    "teleopAmpMissed" : -1, 
     "teleopSpeakerMade" : "Teleop Shots Made Speaker", 
-    "teleopSpeakerMissed" : 0, 
+    "teleopSpeakerMissed" : -1, 
     "notesFed" : "Lobbing", 
     'trapMade' : "Shots Made Trap", 
-    "trapMissed" : 0, 
+    "trapMissed" : -1, 
     "climbed" : "Climbed", 
     "buddyClimb" : "Buddy Climb", 
     "brokeDown" : "Broke Down", 
@@ -32,15 +32,20 @@ inputToOutputIndexes = []
 for inputColumn in INPUT_CONVERSION:
     fullCSV[0].append(inputColumn)
     outputColumn = INPUT_CONVERSION.get(inputColumn)
-    if (outputColumn != 0):
+    if (outputColumn != -1):
         outputColumnIndex = inputCSV[0].index(outputColumn)
         inputToOutputIndexes.append(outputColumnIndex)
+    else:
+        inputToOutputIndexes.append(-1)
         
 for inputCSVRowIndex, inputCSVRow in enumerate(inputCSV):
     fullCSV.append([])
     if (inputCSVRowIndex != 0):
         for inputToOutputIndex in inputToOutputIndexes:
-            fullCSV[inputCSVRowIndex].append(inputCSVRow[inputToOutputIndex])
+            if (inputToOutputIndex != -1):
+                fullCSV[inputCSVRowIndex].append(inputCSVRow[inputToOutputIndex])
+            else:
+                fullCSV[inputCSVRowIndex].append(0)
             
 with open(OUTPUT_CSV_DIR, 'w', newline='') as file:
     writer = csv.writer(file)

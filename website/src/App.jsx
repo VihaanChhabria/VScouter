8897161b5cc4838746e984bdead34a9d24a30c03
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-08-19 17:05:00 -0400

    added router

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
-import ProceedBackButton from "./components/ProceedBackButton";
+import MainLayout from "./layouts/MainLayout"
+
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <ProceedBackButton />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        {/* <Route path="/jobs" element={<JobsPage />} /> */}
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ website/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProceedBackButton from "../components/ProceedBackButton";
+
+const HomePage = () => {
+  return (
+    <body bgcolor="red">
+      <ProceedBackButton />
+    </body>
+  );
+};
+
+export default HomePage;

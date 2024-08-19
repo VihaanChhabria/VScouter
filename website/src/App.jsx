e4762c3c73557912eb300d3b5ce9c51706a51dc8
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-08-19 14:21:43 -0400

    added base react, vite, and tailwind css

---
+++ website/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-red-600 text-5xl">Test</h1>
+    </>
+  );
+}
+
+export default App;

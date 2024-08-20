b483b89e1db7ae8a508dddb06730e3da965f8ca7
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-08-19 22:15:57 -0400

    added textinput and from that match number and scouter initials

---
+++ website/src/components/TextInput.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const TextInput = ({ question = "Match Number", coordX = 10, coordY = 179.52 }) => {
+
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div
+        style={{
+          border: "7px solid #1D1E1E",
+          width: "262.16px",
+          height: "85.52px",
+          backgroundColor: "#242424",
+          borderRadius: "15px",
+          position: "absolute",
+          left: `${coordX}px`,
+          top: `${coordY}px`,
+        }}
+      >
+        <h1
+          style={{
+            color: "#FFFFFF",
+            fontSize: "24px",
+            fontWeight: "bold",
+            paddingLeft: "10px",
+          }}
+        >
+          {question}
+        </h1>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={text}
+          onChange={(e) => setText(e.target.value.toUpperCase())}
+          style={{
+            border: "4px solid #1D1E1E",
+            borderRadius: "10px",
+            backgroundColor: "#4A4A4A",
+            color: "#FFFFFF",
+            width: "242.44px",
+            height: "38.18px",
+            marginLeft: "4px",
+            marginTop: "-4.5px",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default TextInput;
--- website/src/pages/HomePage.jsx
+++ website/src/pages/HomePage.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import ProceedBackButton from "../components/ProceedBackButton";
 import SelectAlliance from "../components/SelectAlliance";
+import TextInput from "../components/TextInput";
 
 const HomePage = () => {
   return (
     <>
       <ProceedBackButton />
       <SelectAlliance />
+      <TextInput question="Match Number" coordX={10} coordY={179.52}/>
+      <TextInput question="Scouter Initials" coordX={10} coordY={275}/>
     </>
   );
 };

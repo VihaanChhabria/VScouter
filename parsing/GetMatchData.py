efc9669d900aa27f20383a8a80701d01508f0f82
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2024-10-18 18:44:34 -0400

    fixed issue of other matches like finals being included in suggestions. big!

--- parsing/GetMatchData.py
+++ parsing/GetMatchData.py
@@ -13,25 +13,53 @@ def downloadMatches(event_key, api_key):
     :return: None
     """
     # Get the matches from TBA
-    matches = getEventMatches(event_key, api_key)
+    allMatches = getEventMatches(event_key, api_key)
 
     # Rearrange the matches to be in a more useful format
-    matches_rearranged = []
+    qualMatches = []
+    
+    # Keep track of the match number we are currently on and the index into the list of matches
+    counter = 1
+    matchCounter = 0
 
-    for match_num, match in enumerate(matches):
-        # Get the red and blue alliances for the current match
+    # Iterate through the list of matches
+    while True:
+        # Iterate through the list of matches until we find the next one that matches the current match number
+        while matchCounter < len(allMatches):
+            match = allMatches[matchCounter]
+            if event_key+"_qm"+str(counter) == match["key"]:
+                # If we found it, add it to the list of qualifying matches and
+                # remove it from the list of all matches
+                qualMatches.append(match)
+                allMatches.pop(matchCounter)
+                # Move on to the next match number and reset the index
+                counter = counter + 1
+                matchCounter = 0
+                break
+            # If not, move on to the next match
+            matchCounter += 1
+        # If we've reached the end of the list of matches, break out of the loop
+        if matchCounter != 0:
+            break
+            
+
+    matchesRearranged = []
+    
+    # Iterate through the list of qualifying matches
+    for match in qualMatches:
+        # Get the red and blue teams for the current match
         red_teams, blue_teams = getMatchTeams(match)
 
-        # Add the match to the rearranged list
-        matches_rearranged.append({
-            "matchNum": match_num,
+        # Add the match to the list of rearranged matches with the match number and the red and blue alliances
+        matchesRearranged.append({
+            "matchNum": match["key"].replace(event_key+"_qm", ""),
             "redAlliance": red_teams,
             "blueAlliance": blue_teams
         })
 
     # Save the rearranged matches to a json file
     with open("../data/EventMatches.json", "w") as file:
-        json.dump({"matches": matches_rearranged}, file)
+        json.dump({"matches": matchesRearranged}, file)
         
     # Save the rearranged matches to a csv file
     with open("../data/EventMatches.csv", "w", newline='') as file:
@@ -40,8 +68,8 @@ def downloadMatches(event_key, api_key):
         # Write the header row
         writer.writerow(["matchNumber", "alliance", "teamNumber"])
         
-        # Write each match
-        for match_num, match in enumerate(matches_rearranged):
+        # Iterate through the list of rearranged matches
+        for match_num, match in enumerate(matchesRearranged):
             # Get the red and blue alliances for the current match
             redAlliance = match["redAlliance"]
             blueAlliance = match["blueAlliance"]
@@ -72,6 +100,9 @@ def getEventMatches(event_key, api_key):
     
     # Parse the matches from the JSON response
     matches = json.loads(body)
+    
+    with open("../data/test.json", "w") as file:
+        file.write(body)
     
     return matches
 

511ab4fa5df5344e3ff08b9695ac852cf31063c4
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-01-26 20:17:03 -0500

    added robots.txt and sitemap.xml

--- website/dev-dist/sw.js
+++ website/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ac6e1177'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p1tn3mo263"
+    "revision": "0.3p95311ppoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

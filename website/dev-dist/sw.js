24f35bbc981301ade30780ffbdbd0f8a1a582cc8
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-01-26 19:56:43 -0500

    added seo stuff to package.json

--- website/dev-dist/sw.js
+++ website/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ac6e1177'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d4abjegped"
+    "revision": "0.p1tn3mo263"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

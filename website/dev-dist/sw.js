80cc7e78cfe90648fb75ba2de4fc75811acfbad4
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-02-02 17:39:23 -0500

    disabled zooming

--- website/dev-dist/sw.js
+++ website/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ac6e1177'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.81ho61vhd0g"
+    "revision": "0.24unfjegrj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

59663fd381fb22ff83295013faca40e1f52b5b22
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-02-14 13:06:29 -0500

    fixed red background in scoring pages

--- website/src/components/ScoringComponents/ScoringCoral/ScoringCoralSection.jsx
+++ website/src/components/ScoringComponents/ScoringCoral/ScoringCoralSection.jsx
@@ -5,8 +5,25 @@ import ScoringCoralPlaceMap from "./ScoringCoralPlaceMap";
 import ScoringPickup from "../ScoringPickup";
 import { toast } from "react-toastify";
 
-const ScoringCoralSection = ({ pickData, placeData }) => {
+const ScoringCoralSection = ({
+  pickData,
+  placeData,
+  mode,
+  coralPreloaded,
+  setCoralPreloaded,
+}) => {
   const [pickPositionSelected, setPickPositionSelected] = useState("");
+  const [hideAutoCoralPreload, setHideAutoCoralPreload] = useState(false)
+
+  useEffect(() => {
+    if (coralPreloaded) {
+      if (placeData.find((singlePlaceData) => singlePlaceData.count > 0)) {
+        setHideAutoCoralPreload(true)
+      } else {
+        setHideAutoCoralPreload(false)
+      }
+    }
+  }, [placeData]);
 
   return (
     <div
@@ -23,16 +40,49 @@ const ScoringCoralSection = ({ pickData, placeData }) => {
         alignItems: "center",
       }}
     >
-      <h1
+      <div
         style={{
-          color: "#FFFFFF",
-          fontSize: "5dvh",
-          fontWeight: "bold",
-          marginBottom: 0,
+          width: "90%",
+          height: "20%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        Coral
-      </h1>
+        <h1
+          style={{
+            color: "#FFFFFF",
+            fontSize: "5dvh",
+            fontWeight: "bold",
+            marginBottom: 0,
+          }}
+        >
+          Coral
+        </h1>
+        {mode == "auto" && hideAutoCoralPreload && (
+          <div
+            style={{
+              width: "25%",
+              height: "100%",
+              backgroundColor: coralPreloaded ? "#507144" : "#242424",
+              border: "1.63dvh solid #1D1E1E",
+              borderRadius: "2dvh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              alignSelf: "flex-end",
+            }}
+            onClick={() => setCoralPreloaded(!coralPreloaded)}
+          >
+            <h1
+              style={{ color: "white", fontSize: "3.25dvh", fontWeight: "700" }}
+            >
+              Preload
+            </h1>
+          </div>
+        )}
+      </div>
+
       <div
         style={{ width: "80%", height: "70%", marginBottom: "1.5dvh" }}
         onClick={() => {

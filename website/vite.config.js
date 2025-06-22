1cc732b16888e18b94fac18094333e2a3a9617b1
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-06-22 17:56:05 -0400

    changed base url config to correct location

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ui/",
   plugins: [
     react(),
     VitePWA({
@@ -43,7 +44,6 @@ export default defineConfig({
           },
         ],
       },
-      base: "/ui/",
     }),
   ],
   server: {

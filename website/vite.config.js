6661fd72fb5391ee42d0d893f84c3d5734537b5e
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-06-24 17:37:36 -0400

    made react router also show /ui/ and /

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,55 +1,54 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+  // vite.config.js
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react";
+  import { VitePWA } from "vite-plugin-pwa";
+  import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig(({ mode }) => {
-  const isProd = mode === "production";
+  export default defineConfig(({ mode }) => {
 
-  return {
-    // base: isProd ? "/ui/" : "/",
-    // build: {
-    //   outDir: "dist/ui",
-    // },
-    base: "/",
-    plugins: [
-      react(),
-      VitePWA({
-        registerType: "autoUpdate",
-        devOptions: {
-          enabled: false,
-        },
-        workbox: {
-          runtimeCaching: [
-            {
-              urlPattern: ({ request }) => request.destination === "document",
-              handler: "StaleWhileRevalidate",
-              options: { cacheName: "html-cache" },
-            },
-            {
-              urlPattern: ({ request }) => request.destination === "script",
-              handler: "StaleWhileRevalidate",
-              options: { cacheName: "js-cache" },
-            },
-            {
-              urlPattern: ({ request }) => request.destination === "style",
-              handler: "StaleWhileRevalidate",
-              options: { cacheName: "css-cache" },
-            },
-            {
-              urlPattern: ({ request }) => request.destination === "image",
-              handler: "StaleWhileRevalidate",
-              options: { cacheName: "image-cache" },
-            },
-          ],
-        },
-      }),
-    ],
-    server: {
-      host: "localhost",
-      port: 3000,
-      strictPort: true,
-    },
-  };
-});
+    return {
+      // base: isProd ? "/ui/" : "/",
+      // build: {
+      //   outDir: "dist/ui",
+      // },
+      base: "/",
+      plugins: [
+        react(),
+        VitePWA({
+          registerType: "autoUpdate",
+          devOptions: {
+            enabled: false,
+          },
+          workbox: {
+            runtimeCaching: [
+              {
+                urlPattern: ({ request }) => request.destination === "document",
+                handler: "StaleWhileRevalidate",
+                options: { cacheName: "html-cache" },
+              },
+              {
+                urlPattern: ({ request }) => request.destination === "script",
+                handler: "StaleWhileRevalidate",
+                options: { cacheName: "js-cache" },
+              },
+              {
+                urlPattern: ({ request }) => request.destination === "style",
+                handler: "StaleWhileRevalidate",
+                options: { cacheName: "css-cache" },
+              },
+              {
+                urlPattern: ({ request }) => request.destination === "image",
+                handler: "StaleWhileRevalidate",
+                options: { cacheName: "image-cache" },
+              },
+            ],
+          },
+        }),
+      ],
+      server: {
+        host: "localhost",
+        port: 3000,
+        strictPort: true,
+      },
+    };
+  });

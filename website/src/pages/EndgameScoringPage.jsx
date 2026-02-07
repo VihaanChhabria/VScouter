e7d97655c62f6dd5720190fd7f04b03c6078c0c9
Vihaan Chhabria <chhabria.vihaan2@gmail.com>
2026-02-06 23:34:33 -0500

    added broken down endgame

--- website/src/pages/EndgameScoringPage.jsx
+++ website/src/pages/EndgameScoringPage.jsx
@@ -1,54 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
-import ProceedBackButton from "../components/ProceedBackButton";
-import EndgameScoringBargeSection from "../components/EndgameScoringComponents/EndgameScoringBargeSection";
-import EndgameScoringToggle from "../components/EndgameScoringComponents/EndgameScoringToggle";
-import EndgameScoringComments from "../components/EndgameScoringComponents/EndgameScoringComments";
+import EndgamePageControlSection from "../components/EndgameScoringComponents/EndgamePageControlSection";
+import ToggleButton from "../components/ToggleButton";
+import SelectOptions from "../components/SelectOptions";
 
 const EndgameScoringPage = () => {
   const location = useLocation();
   const states = location.state;
 
-  const [shallowClimbAttempted, setShallowClimbAttempted] = useState(
-    states?.inputs?.shallowClimbAttempted || false
-  );
-  const [deepClimbAttempted, setDeepClimbAttempted] = useState(
-    states?.inputs?.deepClimbAttempted || false
-  );
-  const [parkAttempted, setParkAttempted] = useState(
-    states?.inputs?.parkAttempted || false
-  );
-
-  const climbData = [
-    {
-      position: "Shallow Climb",
-      selected: shallowClimbAttempted,
-      setSelected: setShallowClimbAttempted,
-    },
-    {
-      position: "Deep Climb",
-      selected: deepClimbAttempted,
-      setSelected: setDeepClimbAttempted,
-    },
-    {
-      position: "Park",
-      selected: parkAttempted,
-      setSelected: setParkAttempted,
-    },
-  ];
-
-  const [climbFailed, setClimbFailed] = useState(
-    states?.inputs?.climbFailed || false
-  );
-  const [playedDefense, setPlayedDefense] = useState(
-    states?.inputs?.playedDefense || false
-  );
-  const [brokeDown, setBrokeDown] = useState(
-    states?.inputs?.brokeDown || false
-  );
-
   const [comment, setComment] = useState(states?.inputs?.comment || "");
+
+  const [brokenDown, setBrokenDown] = useState(states?.inputs?.broken || false);
+  const [brokenDownTime, setBrokenDownTime] = useState(
+    states?.inputs?.brokenDownTime || null,
+  );
 
   return (
     <div
@@ -56,128 +22,134 @@ const EndgameScoringPage = () => {
         width: "100%",
         height: "100%",
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        gap: "2dvh", //edit this
-        padding: "4dvh 2dvw", //edit this
+        gap: "2dvh",
+        padding: "4dvh 2dvw",
       }}
     >
-      <div style={{ flex: "0.67", width: "100%" }}>
-        <EndgameScoringBargeSection climbData={climbData} />
+      <div
+        style={{
+          height: "100%",
+          flex: 0.75 / 2,
+          backgroundColor: "#3B3B3B",
+          borderColor: "#1D1E1E",
+          borderWidth: "2dvh",
+          borderRadius: "3.49dvh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1
+          style={{
+            color: "#FFFFFF",
+            fontSize: "5.5dvh",
+            fontWeight: "bold",
+          }}
+        >
+          Defense
+        </h1>
       </div>
       <div
         style={{
-          flex: "0.45",
-          width: "85%",
+          height: "100%",
+          flex: 0.75 / 2,
+          backgroundColor: "#3B3B3B",
+          borderColor: "#1D1E1E",
+          borderWidth: "2dvh",
+          borderRadius: "3.49dvh",
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap: "1dvw",
         }}
       >
-        <div style={{ flex: "1", height: "100%" }}>
-          <EndgameScoringToggle
-            question={"Climbed Failed"}
-            selected={climbFailed}
-            setSelected={setClimbFailed}
+        <h1
+          style={{
+            color: "#FFFFFF",
+            fontSize: "5.5dvh",
+            fontWeight: "bold",
+            marginBottom: "2dvh",
+            marginTop: "1dvh",
+          }}
+        >
+          Broken
+        </h1>
+
+        <div
+          style={{
+            paddingLeft: "1.5dvw",
+            paddingRight: "1.5dvw",
+            width: "100%",
+            height: "20%",
+          }}
+        >
+          <ToggleButton
+            question={"Broken Down?"}
+            selected={brokenDown}
+            setSelected={(value) => setBrokenDown(value)}
+            fontSize={"4dvh"}
           />
         </div>
-        <div style={{ flex: "1", height: "100%" }}>
-          <EndgameScoringToggle
-            question={"Played Defense"}
-            selected={playedDefense}
-            setSelected={setPlayedDefense}
-          />
-        </div>
-        <div style={{ flex: "1", height: "100%" }}>
-          <EndgameScoringToggle
-            question={"Broke Down"}
-            selected={brokeDown}
-            setSelected={setBrokeDown}
-          />
-        </div>
+
+        {brokenDown && (
+          <div style={{ width: "100%", height: "80%", padding: "0 1.5dvw" }}>
+            <h1
+              style={{
+                color: "#FFFFFF",
+                fontSize: "3dvh",
+                fontWeight: "500",
+                margin: "1.5dvh 0",
+              }}
+            >
+              How Long Was the Robot Broken Down For?
+            </h1>
+            <div style={{ width: "100%", height: "75%" }}>
+              <SelectOptions
+                optionsData={["Shortly", "A Lot", "Whole Match"]}
+                optionSelected={brokenDownTime}
+                setOptionSelected={(value) => setBrokenDownTime(value)}
+                flexDirection={"column"}
+              />
+            </div>
+          </div>
+        )}
       </div>
       <div
         style={{
-          flex: "1",
-          width: "100%",
+          height: "100%",
+          flex: 0.25,
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap: "1.5dvw",
+          gap: "1dvh",
         }}
       >
-        <div
+        <textarea
           style={{
-            height: "100%",
-            width: "75%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            width: "100%",
+            flex: 0.6,
+            border: "0.93dvh solid #1D1E1E",
+            borderRadius: "2.33dvh",
+            backgroundColor: "#4A4A4A",
+            color: "#FFFFFF",
+            fontSize: "3.0dvh",
+            padding: "1.56dvh",
           }}
-        >
-          <div style={{ width: "100%", flex: "1" }}>
-            <EndgameScoringComments comment={comment} setComment={setComment} />
-          </div>
-          <h1
-            style={{
-              color: "#FFFFFF",
-              fontSize: "9dvh",
-              fontWeight: "bold",
-              width: "100%",
-              flex: "0.5",
-            }}
-          >
-            Endgame Scoring
-          </h1>
-        </div>
-        <div
-          style={{
-            height: "100%",
-            width: "25%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "2dvh",
-          }}
-        >
-          <div style={{ flex: "0.25", width: "100%" }}>
-            <ProceedBackButton
-              back={true}
-              nextPage="teleop-scoring"
-              inputs={{
-                ...(states?.inputs || {}),
-                shallowClimbAttempted,
-                deepClimbAttempted,
-                parkAttempted,
-                climbFailed,
-                playedDefense,
-                brokeDown,
-                comment,
-              }}
-            />
-          </div>
-          <div style={{ flex: "1", width: "100%" }}>
-            <ProceedBackButton
-              nextPage={`game-start`}
-              inputs={{
-                ...(states?.inputs || {}),
-                shallowClimbAttempted,
-                deepClimbAttempted,
-                parkAttempted,
-                climbFailed,
-                playedDefense,
-                brokeDown,
-                comment,
-              }}
-              message={"Submit"}
-            />
-          </div>
+          onChange={(e) => setComment(e.target.value)}
+          id="comments"
+          defaultValue={comment}
+          placeholder="Enter Comments Here"
+        />
+        <div style={{ flex: 0.4, width: "100%" }}>
+          <EndgamePageControlSection
+            states={states}
+            extraInputs={{ comment }}
+          />
         </div>
       </div>
     </div>
